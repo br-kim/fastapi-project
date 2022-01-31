fc8a6dcfd42a30043cece89127c1efe329075dee
idle947 <idle947@gmail.com>
2022-01-31 13:58:36 +0900

    프로젝트 환경설정

---
+++ app/config.py
@@ -0,0 +1,11 @@
+import os
+
+env = os.getenv("FASTAPI_ENV")
+if env == "production":
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+asyncpg://", 1)
+
+elif env == "local" or env is None:
+    DATABASE_URL = "postgresql+asyncpg://postgres:1234@localhost/sample_app"
+
