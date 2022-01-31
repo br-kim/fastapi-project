7ecc2976329bc4782fddb6ccaeaf789217b7e6bb
idle947 <idle947@gmail.com>
2022-01-31 14:00:43 +0900

    main 코드 수정

--- app/main.py
+++ app/main.py
@@ -2,10 +2,21 @@ import uvicorn
 from fastapi import FastAPI
 
 from app.router.index import index_router
+from app.router.posts import posts_router
+from app.db.models import Base
+from app.db.database import engine
+
 
 app = FastAPI()
 app.include_router(index_router)
+app.include_router(posts_router, prefix="/posts")
+
+
+@app.on_event("startup")
+async def startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app")
+    uvicorn.run("main:app", reload=True)
