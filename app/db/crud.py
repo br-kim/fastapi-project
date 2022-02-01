34144b1a946b4fbdafbce7609897bf19c40549fc
idle947 <idle947@gmail.com>
2022-02-01 16:14:59 +0900

    타입 힌트 제거

--- app/db/crud.py
+++ app/db/crud.py
@@ -1,17 +1,16 @@
-from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy import insert
 
 from app.db.models import Post
 
 
-async def get_post_db(session: AsyncSession, post_num):
+async def get_post_db(session, post_num):
     stmt = select(Post).where(Post.id == post_num)
     post = await session.execute(stmt)
     return post.scalar()
 
 
-async def write_post_db(session: AsyncSession, post):
+async def write_post_db(session, post):
     stmt = insert(Post).values(post.dict())
     result = await session.execute(stmt)
     return result.inserted_primary_key
